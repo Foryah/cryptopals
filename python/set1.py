2529116ee0e281bf4690c576068839643a527ba2
David Suilea <david.suilea@gmail.com>
2016-05-08 21:46:09 +0200

    Python challange 2

--- python/set1.py
+++ python/set1.py
@@ -2,10 +2,35 @@ import base64
 import binascii
 
 
-def hex2bin_str(hex_str):
+def hex2hex_str(hex_val):
+    return str(hex_val[2:])
+
+
+def hex_str2hex(hex_str):
+    return int(hex_str, 16)
+
+
+def bin_str2hex_str(bin_str):
+    return binascii.hexlify(bin_str)
+
+
+def hex_str2bin_str(hex_str):
     return binascii.unhexlify(hex_str)
 
 
-def hex2b64(hex_str):
-    bin_str = hex2bin_str(hex_str)
+def hex_str2b64_str(hex_str):
+    bin_str = hex_str2bin_str(hex_str)
     return base64.b64encode(bin_str)
+
+
+def heXor(hex_str1, hex_str2):
+    if len(hex_str1) != len(hex_str2):
+        raise ValueError("The two strings have different length")
+
+    hex_val1 = hex_str2hex(hex_str1)
+    hex_val2 = hex_str2hex(hex_str2)
+
+    hex_result = hex(hex_val1 ^ hex_val2)
+    hex_str_result = hex2hex_str(hex_result)
+
+    return hex_str_result
--- python/test_set1.py
+++ python/test_set1.py
@@ -5,11 +5,17 @@ import unittest
 class TestSet1(unittest.TestCase):
 
     def test_challange1(self):
-        bin_str = set1.hex2bin_str("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+        bin_str = set1.hex_str2bin_str("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
         self.assertEqual(bin_str.decode("ascii"), "I'm killing your brain like a poisonous mushroom")
 
-        bin_str = set1.hex2b64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+        bin_str = set1.hex_str2b64_str("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
         self.assertEqual(bin_str.decode("ascii"), "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+
+    def test_challange2(self):
+        str1 = "1c0111001f010100061a024b53535009181c"
+        str2 = "686974207468652062756c6c277320657965"
+
+        self.assertEqual(set1.heXor(str1, str2), "746865206b696420646f6e277420706c6179")
 
 if __name__ == '__main__':
     unittest.main()
