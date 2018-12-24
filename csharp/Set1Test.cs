011abc1c0b85afd2426157250dcb51d86eb784c2
David Suilea <david.suilea@gmail.com>
2018-12-24 18:09:06 +0100

    Solve second challange

--- csharp/Set1Test.cs
+++ csharp/Set1Test.cs
@@ -26,4 +26,13 @@ public class Set1Test
             )
         );
     }
+
+    [Fact]
+    public void Xor_two_hex_strings()
+    {
+        Assert.Equal(
+            "746865206B696420646F6E277420706C6179",
+            Solver.XorEqualHexes( "1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
+        );
+    }
 }
